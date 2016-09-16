41d6d8e4d74a9fe20c041da9c307907cef0a95a4
lavrov <lavrovvv@gmail.com>
2016-09-16 16:25:38 +0200

    add test for library example

--- src/test/scala/com/github/lavrov/xml/reader/GenericReaderSpec.scala
+++ src/test/scala/com/github/lavrov/xml/reader/GenericReaderSpec.scala
@@ -33,6 +33,44 @@ class GenericReaderSpec extends WordSpec {
         Book("Programming in Scala", 2016, Chapter("One") :: Chapter("Two") :: Nil)
       )
     }
+
+    "generate reader for library case class" in {
+      case class Author(name: String)
+      case class Book(title: String, year: Int, author: List[Author])
+      case class Library(book: List[Book])
+
+      import XmlPath.__
+      import GenericReader._
+
+      implicit val authorReader: Reader[Author] =
+        __.read[String] map Author
+      implicit val bookReader = Reader.reader[Book]
+      val libraryReader = Reader.reader[Library]
+
+      val result =
+        libraryReader(
+          <library>
+            <book>
+              <title>Programming in Scala, Third Edition</title>
+              <year>2016</year>
+              <author>Martin Odersky</author>
+              <author>Lex Spoon</author>
+              <author>Bill Venners</author>
+            </book>
+          </library>
+        )
+      result shouldBe valid(
+        Library(
+          Book(
+            "Programming in Scala, Third Edition",
+            2016,
+              Author("Martin Odersky") ::
+              Author("Lex Spoon") ::
+              Author("Bill Venners") :: Nil
+          ) :: Nil
+        )
+      )
+    }
   }
 }
 
